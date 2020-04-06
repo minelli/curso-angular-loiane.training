89c3b84edfc175adbae56c21af55a0ec0ea66215
Marcelo Minelli <mrminelli@live.com>
2020-04-06 11:29:50 -0400

    Curso Loiane.training: Parte 4: Serviços

---
+++ services/src/app/cursos/cursos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { LogService } from '../shared/log.service';
+
+@Injectable()
+export class CursosService {
+
+    constructor(private logService: LogService) {
+        console.log('CursosService');
+    }
+
+    static criouNovoCurso = new EventEmitter<string>();
+    emitirCursoCriado = new EventEmitter<string>();
+    private cursos: string[] = ['Angular2', ' Java', 'Phonegap'];
+
+    getCursos() {
+        this.logService.consoleLog('Obtendo lista de cursos');
+        return this.cursos;
+    }
+
+    addCurso(curso: string) {
+        this.logService.consoleLog(`Criando um novo curso ${curso}`);
+        this.cursos.push(curso);
+        this.emitirCursoCriado.emit(curso);
+        CursosService.criouNovoCurso.emit(curso);
+    }
+}
